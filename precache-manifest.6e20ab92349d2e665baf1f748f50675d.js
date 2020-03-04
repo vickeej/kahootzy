88b57aabf4a1209d6d90d5625989ac2437f8e918
Vickee Johnson <vi436649@ucf.edu>
2020-03-04 12:35:26 -0500

    Updates

---
+++ precache-manifest.6e20ab92349d2e665baf1f748f50675d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281528cd0156c6d7781b99b42a2ef8b9",
+    "url": "/kahootzy/index.html"
+  },
+  {
+    "revision": "248643e690608b553161",
+    "url": "/kahootzy/static/css/main.41a37e57.chunk.css"
+  },
+  {
+    "revision": "9a6db4474f5cce18a998",
+    "url": "/kahootzy/static/js/2.589f4916.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kahootzy/static/js/2.589f4916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248643e690608b553161",
+    "url": "/kahootzy/static/js/main.3827602b.chunk.js"
+  },
+  {
+    "revision": "2277703a9e931267cb1c",
+    "url": "/kahootzy/static/js/runtime-main.7c1e51b0.js"
+  }
+]);
