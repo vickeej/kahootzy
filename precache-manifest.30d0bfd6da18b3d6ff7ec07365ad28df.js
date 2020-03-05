1d08452f54be27e6df279864e1e499dcaa02bb9f
Vickee Johnson <vickeejohnson@knights.ucf.edu>
2020-03-04 23:54:41 -0500

    Updates

---
+++ precache-manifest.30d0bfd6da18b3d6ff7ec07365ad28df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63f4c818c85b199d6c32ef92815dfd8",
+    "url": "/kahootzy/index.html"
+  },
+  {
+    "revision": "372f3b60c7a4af2c591c",
+    "url": "/kahootzy/static/css/main.2c055304.chunk.css"
+  },
+  {
+    "revision": "7a0b156a25ab93e6ae5b",
+    "url": "/kahootzy/static/js/2.8b98a2d4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kahootzy/static/js/2.8b98a2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372f3b60c7a4af2c591c",
+    "url": "/kahootzy/static/js/main.6c1551a3.chunk.js"
+  },
+  {
+    "revision": "2277703a9e931267cb1c",
+    "url": "/kahootzy/static/js/runtime-main.7c1e51b0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kahootzy/precache-manifest.6e20ab92349d2e665baf1f748f50675d.js"
+  "/kahootzy/precache-manifest.30d0bfd6da18b3d6ff7ec07365ad28df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kahootzy/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
